3ce2309b7e174bacd7e3553c3c332673222f4d82
Maznu007 <hasnatrafiuddinmunif@gmail.com>
2025-12-14 18:08:21 +0600

    almost

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ lists.forEach(list => {
     list.addEventListener('drop', dragDrop);
 });
 
+function dragStart(e){
+    e.dataTransfer.setData('text/plain', e.target.id);
+}
+function dragEnd(e){
+    console.log('Drag Ended');
+}
+function dragOver(e){
+    e.preventDefault();
+}
+function dragEnter(e){
+    e.preventDefault();
+    e.target.classList.add('over');
+}
+function dragLeave(e){
+    this.classList.remove('over');
+}
+function dragDrop(e){
+    const id = e.dataTransfer.getData('text/plain');
+    const draggableElement = document.getElementById(id);
+    e.target.appendChild(draggableElement);
+    e.target.classList.remove('over');
+}
